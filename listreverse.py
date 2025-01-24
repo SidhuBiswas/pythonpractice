c4b6a31b28dd5f95c048a5b52567056c019657cf
Siddhartho Biswas <siddharthobiswas2018@gmail.com>
2025-01-23 17:04:03 -1200

    Update listreverse.py

--- listreverse.py
+++ listreverse.py
@@ -11,14 +11,4 @@ apple.reverse()
 if apple == apple1:
     print("Both are equal")
 else:
-    print("Not equal")
-
-'''def gcd(a,b):
-    if b == 0:
-        return a
-    return(b,a%b)
-
-n = int(input("enter 1st number:"))
-i = int(input("Enter 2nd number:"))
-s = gcd(48,18)
-print("GCD = ",s)'''
+    print("Both are not equal")
